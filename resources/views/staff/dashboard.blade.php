abdaab9b4b0639a11a84041563e22aafb8a15419
dutautes <dutasuksesif@gmail.com>
2025-10-01 08:18:56 +0700

    add : notif in dashboards

--- resources/views/staff/dashboard.blade.php
+++ resources/views/staff/dashboard.blade.php
@@ -1,6 +1,12 @@
 @extends('templates.app')
 
 @section('content')
+    @if (Session::get('success'))
+        {{-- Auth::user() : mengambil data pengguna yang login --}}
+        {{-- format : Auth:user()->column_di_fillable --}}
+        <div class="alert alert-success w-100">{{ Session::get('success') }} <b>Selamat datang, {{ Auth::user()->name }}</b>
+        </div>
+    @endif
     <div class="container mt-5">
         <h5>Dashboard Petugas</h5>
     </div>
